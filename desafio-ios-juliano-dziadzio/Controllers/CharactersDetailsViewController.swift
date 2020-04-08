84535152e088eebfd7fc924c5c062db0e6056c38
juliano <julianojd@ymail.com>
2020-04-07 23:04:21 -0300

    binds da tela de detalhes e constrains iniciais

--- desafio-ios-juliano-dziadzio/Controllers/CharactersDetailsViewController.swift
+++ desafio-ios-juliano-dziadzio/Controllers/CharactersDetailsViewController.swift
@@ -1,8 +1,18 @@
 import UIKit
 
 class CharactersDetailsViewController: UIViewController, Storyboarded {
-   weak var coordinator: MainCoordinator?
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var shortResume: UILabel!
+    
+    weak var coordinator: MainCoordinator?
+    var character: CharacterModel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let charcter = character {
+            self.name.text = charcter.name
+            self.shortResume.text = charcter.description
+        }
     }
 }
