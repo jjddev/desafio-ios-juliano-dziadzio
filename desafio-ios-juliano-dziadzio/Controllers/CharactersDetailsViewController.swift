cc7403d3babc74c40d77cc196684c43b1e9f591d
juliano <julianojd@ymail.com>
2020-04-07 22:18:27 -0300

    ViewController de detalhes do personagem

---
+++ desafio-ios-juliano-dziadzio/Controllers/CharactersDetailsViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class CharactersDetailsViewController: UIViewController, Storyboarded {
+   weak var coordinator: MainCoordinator?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
