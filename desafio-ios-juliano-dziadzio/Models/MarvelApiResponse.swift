8d979af50ef06464a3942280abc72909d8cd8269
juliano <julianojd@ymail.com>
2020-04-07 22:34:19 -0300

    Model especifico para o personagem e struct da imagem

--- desafio-ios-juliano-dziadzio/Models/MarvelApiResponse.swift
+++ desafio-ios-juliano-dziadzio/Models/MarvelApiResponse.swift
@@ -13,5 +13,23 @@ struct Character: Codable {
     let id: Int
     let name: String
     let description: String
+    let thumbnail: Thumbnail
+    
+    
+    func toModel() -> CharacterModel {
+        let path = "\(self.thumbnail.path)/standard_medium.\(self.thumbnail.fileExtension)"
+        return CharacterModel(id: self.id, name: self.name, description: self.description, imagePath: path, imageBlob: "")
+    }
+    
+}
+
+struct Thumbnail: Codable {
+    let path: String
+    let fileExtension: String
+    
+    enum CodingKeys: String, CodingKey {
+        case fileExtension = "extension"
+        case path
+    }
 }
 
