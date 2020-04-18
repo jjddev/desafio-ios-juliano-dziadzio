4516b2976bbe22b5b06964e1a13428f9fc1b5918
juliano <julianojd@ymail.com>
2020-04-17 22:49:42 -0300

    Removed  fetchCharacters and fetchComics, were replaced by fetchData

--- desafio-ios-juliano-dziadzio/Service/Service.swift
+++ desafio-ios-juliano-dziadzio/Service/Service.swift
@@ -32,51 +32,6 @@ class Service {
         task.resume()
     }
     
-    func fetchCharacters<T: Codable>(resultType: T.Type, completionHandler: @escaping (_  result: T) -> Void) {
-        
-        let token = generateToken()
-        let urls = AppSettings.ApiUrl + "characters?ts=\(token.timestamp)&apikey=\(AppSettings.ApiPublicKey)&hash=\(token.value)&limit=15&offset=0"
-        
-        let url = URL(string: urls)!
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        
-        let task = URLSession.shared.dataTask(with: url) { result in
-            switch result {
-                case .success(let data):
-                    let json = try! JSONDecoder().decode(T.self, from: data)
-                    completionHandler(json)
-                case .failure(let error):
-                    NSLog(error.localizedDescription)
-                    //completionHandler("")
-            }
-        }
-        
-        task.resume()
-    }
-    
-    func fetchComics(characterId: Int, completionHandler: @escaping (_ result: ComicResponse) -> Void) {
-        let token = generateToken()
-        let urls = AppSettings.ApiUrl + "characters/\(characterId)/comics?ts=\(token.timestamp)&apikey=\(AppSettings.ApiPublicKey)&hash=\(token.value)"
-        
-        let url = URL(string: urls)!
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        
-        let task = URLSession.shared.dataTask(with: url) { result in
-            switch result {
-                case .success(let data):
-                    let json = try! JSONDecoder().decode(ComicResponse.self, from: data)
-                    completionHandler(json)
-                case .failure(let error):
-                    NSLog(error.localizedDescription)
-                    //completionHandler("")
-            }
-        }
-        
-        task.resume()
-    }
-    
     func getImage(urlDownload: String, completionHander: @escaping (Data) -> Void) {
         let url = URL(string: urlDownload)!
         
