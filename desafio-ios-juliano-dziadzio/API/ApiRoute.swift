21ecc5d66ca868ed2ce5d677f978ad1b835e1c94
juliano <julianojd@ymail.com>
2020-04-22 22:08:42 -0300

    Moved generateToken() to ApiRoute

--- desafio-ios-juliano-dziadzio/API/ApiRoute.swift
+++ desafio-ios-juliano-dziadzio/API/ApiRoute.swift
@@ -1,19 +1,31 @@
 import Foundation
 
 enum ApiRoute {
-    case characters
+    case characters(Int = 15, Int = 0)
     case comics(Int)
     case image(String, String, String)
     
     
     var route: String {
+        
+        let token = generateToken()
+        
         switch self {
-        case .characters:
-            return AppSettings.ApiUrl + "characters"
+        case .characters(let limit, let offset):
+            return AppSettings.ApiUrl + "characters" + "?ts=\(token.timestamp)&apikey=\(AppSettings.ApiPublicKey)&hash=\(token.value)&limit=\(limit)&offset=\(offset)"
         case .comics(let characterId):
             return AppSettings.ApiUrl + "characters/\(characterId)/comics"
         case .image(let path, let size, let fileExtension):
             return "\(path)/\(size).\(fileExtension)"
         }
     }
+    
+    
+    private func generateToken() -> Token {
+        let timestamp = String(Date().timeIntervalSince1970)
+        let token = "\(timestamp)\(AppSettings.ApiPrivateKey)\(AppSettings.ApiPublicKey)".insecureMD5Hash() ?? ""
+        
+        return Token(value: token, timestamp: timestamp)
+    }
+    
 }
